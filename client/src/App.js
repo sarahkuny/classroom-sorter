ddf83d46420126d830b44674cff601e6f3e48283
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-05-09 16:36:51 -0500

    create groupview, rosterview components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
+import GroupView from "./components/GroupView";
+import RosterView from "./components/RosterView";
 
 function App() {
   return (
     <div className="App">
-      
+      <GroupView />
+      <RosterView />
     </div>
   );
 }
--- model/database.js
+++ model/database.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "todos",
+  database: DB_NAME || "sorter",
   multipleStatements: true
 });
 
@@ -18,15 +18,21 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id));";
-  con.query(sql, function (err, result) {
+  //create students table
+  let sqlStudents = "DROP TABLE if exists students; CREATE TABLE students(id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255) not null, last_name VARCHAR(255), group_id INT, PRIMARY KEY (id));";
+  con.query(sqlStudents, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `items` was successful!");
+    console.log("Table creation `students` was successful!");
+
+  });
+  //create behaviors table
+  let sqlBehaviors = "DROP TABLE if exists behaviors; CREATE TABLE behaviors(id INT NOT NULL AUTO_INCREMENT, student_id INT, has_goal_one BOOLEAN not null, has_goal_two BOOLEAN not null, has_goal_three BOOLEAN not null, PRIMARY KEY (id), FOREIGN KEY (student_id) REFERENCES students(id));";
+  con.query(sqlBehaviors, function (err, result) {
+    if (err) throw err;
+    console.log("Table creation `behaviors` was successful!");
 
     console.log("Closing...");
   });
-
   con.end();
 });
-
 
