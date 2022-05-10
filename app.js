d15e1657acd1170e6a1cff7257cca1a539d594a2
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-05-10 14:24:29 -0500

    add api route to app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
+
 
 var app = express();
 
@@ -16,8 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", function(req, res, next) {
+  res.send("Access the API at path/api");
+});
+
+app.use('/api', apiRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
