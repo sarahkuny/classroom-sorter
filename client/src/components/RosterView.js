022e2da59f17dfe5cc0bf126ded98be5c92f6bf4
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-05-09 19:29:16 -0500

    add form to rosterView

--- client/src/components/RosterView.js
+++ client/src/components/RosterView.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function RosterView() {
-  return (
-    <div>RosterView</div>
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [hasGoalOne, setHasGoalOne] = useState(false);
+  const [hasGoalTwo, setHasGoalTwo] = useState(false);
+  const [hasGoalThree, setHasGoalThree] = useState(false);
+  
+  const handleFirstNameChange = (event) => {
+    let input = event.target.value;
+    setFirstName(input);
+  };
+
+  const handleLastNameChange = (event) => { 
+    let input = event.target.value;
+    setLastName(input);
+  }
+
+  const handleCheckOne = (event) => {
+      setHasGoalOne(event.target.checked)
+  }
+
+  const handleCheckTwo = (event) => {
+      setHasGoalTwo(event.target.checked)
+  }
+
+  const handleCheckThree = (event) => {
+      setHasGoalThree(event.target.checked)
+  }
+
+  const handleAddStudent = (e) => {
+      e.preventDefault();
+      //post request to students
+      //setStudents(response)
+      //post request to behaviors
+      setFirstName("");
+      setLastName("");
+      
+  }
+    return (
+    <div>
+        <form>
+            <div className="name-input">
+                <label>First Name</label>
+                <input onChange={handleFirstNameChange} value={firstName}></input>
+            </div>
+            <div className="name-input">
+                <label>Last Name</label>
+                <input onChange={handleLastNameChange} value={lastName}></input>
+            </div>
+            <div className="goal-selection" onClick={handleCheckOne}>
+                <label>I can make good choices even if I am mad.</label>
+                <input type="checkbox"></input>
+            </div>
+            <div className="goal-selection" onClick={handleCheckTwo}>
+                <label>I can be okay even if others are not okay.</label>
+                <input type="checkbox"></input>
+            </div>
+            <div className="goal-selection">
+                <label>I can do something even if I don't want to (or it's hard).</label>
+                <input type="checkbox" onClick={handleCheckThree}></input>
+            </div>
+            <button onClick={handleAddStudent}>Add Student</button>
+        </form>
+    </div>
   )
 }
