2c50f9ca93afebc1ed7a2580874485e131e30e29
Sarah Kuny <sarahkuny@icloud.com>
2022-05-11 14:57:47 -0500

    complete post method

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@ const sendAllStudents = (req, res) => {
   };
 
 const sendAllStudentsJoined = (req, res) => {
-    db("SELECT students.id, students.first_name, students.last_name, students.group_id, behaviors.id, behaviors.has_goal_one, behaviors.has_goal_two, behaviors.has_goal_three FROM students, behaviors WHERE students.id=behaviors.student_id;")
+    db(`SELECT students.first_name, students.last_name, students.group_id, behaviors.has_goal_one, behaviors.has_goal_two, behaviors.has_goal_three FROM students INNER JOIN behaviors ON students.id = behaviors.student_id;`)
         .then(results => {
             res.send(results.data);
         })
@@ -22,17 +22,48 @@ const sendAllStudentsJoined = (req, res) => {
 router.get('/', function(req, res, next) {
   res.send("Welcome to the API");
 });
-
+//get all students from student table
 router.get('/students', (req, res) => {
     sendAllStudents(req, res)
 })
-
-router.post('/students', (req, res) => {
-    db(`INSERT INTO students (first_name, last_name) VALUES ("${req.body.first_name}", "${req.body.last_name}");`)
+//delete student by id from students table
+router.delete('/students/:id', (req,res) => {
+  db(`DELETE from students WHERE id = ${req.params.id};`)
     .then(() => {
-        sendAllStudents(req, res);
-    })
-    .catch(err => res.status(500).send(err))
+      db(`SELECT * FROM students;`)
+        .then((results) => {
+          res.send(results.data)
+        })
+        .catch(err => res.status(500).send(err));
+    });
+});
+
+router.get('/students/joined', (req, res) => {
+  sendAllStudentsJoined(req, res)
 })
 
+//add student to students and behaviors table, return join table
+router.post ('/students', (req, res) => {
+    db(`INSERT INTO students (first_name, last_name) VALUES ("${req.body.first_name}", "${req.body.last_name}");`)
+      .then(() => {
+        db(`SELECT id FROM students WHERE last_name="${req.body.last_name}" AND first_name="${req.body.first_name}"`)
+          .then((results) => {
+            db(`INSERT INTO behaviors (has_goal_one, has_goal_two, has_goal_three, student_id) SELECT ${req.body.has_goal_one}, ${req.body.has_goal_two}, ${req.body.has_goal_three}, ${results.data[0].id} FROM students WHERE last_name="${req.body.last_name}" AND first_name="${req.body.first_name}";`)
+              .then(() => {
+                db(`SELECT students.first_name, students.last_name, behaviors.has_goal_one, behaviors.has_goal_two, behaviors.has_goal_three FROM students INNER JOIN behaviors ON students.id = behaviors.student_id;`)
+                  .then((results) => {
+                    res.send(results.data)
+                  })
+              })
+          })
+      })
+      .catch((error) => {
+        res.status(500).send(error)
+      })
+    });
+    
+    
+    
+    
+  
 module.exports = router;
