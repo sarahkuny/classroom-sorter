af39a9bed1026964073095141c64dd596a9bcafb
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-05-11 18:21:57 -0500

    organize function order

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
 
+//helper functions
 const sendAllStudents = (req, res) => {
     db("SELECT * FROM students ORDER BY id ASC;")
       .then(results => {
@@ -16,16 +17,53 @@ const sendAllStudentsJoined = (req, res) => {
             res.send(results.data);
         })
         .catch(err => res.status(500).send(err))
-}
+};
 
-/* GET home page. */
+
+//GET METHODS
+// GET home page
 router.get('/', function(req, res, next) {
   res.send("Welcome to the API");
 });
-//get all students from student table
+
+//GET all students from student table
 router.get('/students', (req, res) => {
     sendAllStudents(req, res)
-})
+});
+
+//GET students/behavior join table
+router.get('/students/joined', (req, res) => {
+  sendAllStudentsJoined(req, res)
+});
+
+
+
+//POST METHODS
+//add student to students and behaviors table, return join table
+router.post ('/students', (req, res) => {
+  db(`INSERT INTO students (first_name, last_name) VALUES ("${req.body.first_name}", "${req.body.last_name}");`)
+    .then(() => {
+      db(`SELECT id FROM students WHERE last_name="${req.body.last_name}" AND first_name="${req.body.first_name}"`)
+        .then((results) => {
+          db(`INSERT INTO behaviors (has_goal_one, has_goal_two, has_goal_three, student_id) SELECT ${req.body.has_goal_one}, ${req.body.has_goal_two}, ${req.body.has_goal_three}, ${results.data[0].id} FROM students WHERE last_name="${req.body.last_name}" AND first_name="${req.body.first_name}";`)
+            .then(() => {
+              db(`SELECT students.first_name, students.last_name, behaviors.has_goal_one, behaviors.has_goal_two, behaviors.has_goal_three FROM students INNER JOIN behaviors ON students.id = behaviors.student_id;`)
+                .then((results) => {
+                  res.send(results.data)
+                })
+            })
+        })
+    })
+    .catch((error) => {
+      res.status(500).send(error)
+    })
+  });
+  
+
+//PUT Methods
+//create later? Use in sorting algorithm?
+
+//DELETE Methods
 //delete student by id from students and behaviors table
 router.delete('/students/:id', (req,res) => {
   db(`DELETE from behaviors WHERE student_id = ${req.params.id};`)
@@ -39,31 +77,9 @@ router.delete('/students/:id', (req,res) => {
       res.status(500).send(error)
     })
 });
-//get students data from 
-router.get('/students/joined', (req, res) => {
-  sendAllStudentsJoined(req, res)
-})
 
-//add student to students and behaviors table, return join table
-router.post ('/students', (req, res) => {
-    db(`INSERT INTO students (first_name, last_name) VALUES ("${req.body.first_name}", "${req.body.last_name}");`)
-      .then(() => {
-        db(`SELECT id FROM students WHERE last_name="${req.body.last_name}" AND first_name="${req.body.first_name}"`)
-          .then((results) => {
-            db(`INSERT INTO behaviors (has_goal_one, has_goal_two, has_goal_three, student_id) SELECT ${req.body.has_goal_one}, ${req.body.has_goal_two}, ${req.body.has_goal_three}, ${results.data[0].id} FROM students WHERE last_name="${req.body.last_name}" AND first_name="${req.body.first_name}";`)
-              .then(() => {
-                db(`SELECT students.first_name, students.last_name, behaviors.has_goal_one, behaviors.has_goal_two, behaviors.has_goal_three FROM students INNER JOIN behaviors ON students.id = behaviors.student_id;`)
-                  .then((results) => {
-                    res.send(results.data)
-                  })
-              })
-          })
-      })
-      .catch((error) => {
-        res.status(500).send(error)
-      })
-    });
-    
+
+
     
     
     
